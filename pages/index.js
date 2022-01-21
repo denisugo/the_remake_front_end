51f1ee37c3107aa21ae186a54356044bf4c34215
denisugo <superden1320@gmail.com>
2022-01-21 15:52:08 +0100

    Minor fixes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Meta from "../components/Head/Meta";
 import Header from "../components/Header/Header";
 import ProductList from "../components/ProductList/ProductList";
 import Search from "../components/Search/Search";
-import { endpoints, routes, jwtConfig } from "../config/constants";
+import { endpoints, jwtConfig } from "../config/constants";
 import NewProduct from "../components/NewProduct/NewProduct";
 
 function Home({ list, user }) {
@@ -17,7 +17,7 @@ function Home({ list, user }) {
       <Meta title="Main page" description="test" />
       {process.env.NEXT_PUBLIC_NODE_ENV === "production" && (
         <div style={{ background: "#d3aa14", padding: 10, margin: 10 }}>
-          <h2>IMPORTANT NOTE</h2>
+          <h2>IMPORTANT NOTE!!!</h2>
           <p>
             {
               "herokuapp.com is included in the Mozilla Foundation’s Public Suffix List. This list is used in recent versions of several browsers, such as Firefox, Chrome and Opera, to limit how broadly a cookie may be scoped. In other words, in browsers that support the functionality, applications in the herokuapp.com domain are prevented from setting cookies for _.herokuapp.com. Note that _.herokuapp.com cookies can currently be set in Internet Explorer, but this behavior should not be relied upon and may change in the future. Therefore this app doesn't have an ability to set all necessary cookies while it is hosted on Heroku with an its default domain."
@@ -39,7 +39,6 @@ function Home({ list, user }) {
 
 export default Home;
 
-//TODO: Add decryption of JWT and retrieve a user
 export const getServerSideProps = async (context) => {
   //* Display all env vars
   if (process.env.NEXT_PUBIC_NODE_ENV === "production") {
@@ -50,20 +49,13 @@ export const getServerSideProps = async (context) => {
       process.env.NEXT_PUBLIC_STRIPE_PK
     );
   }
-  // export const getServerSideProps = wrapper.getServerSideProps(
-  //   (store) => async (context) => {
-  // const connectSidCookie = context.req.cookies["connect.sid"];
-  // const cookie = `connect.sid=${connectSidCookie}`;
+
   //? Setting up the product endpoint
   const endpoint = endpoints.products();
   const url = `${process.env.NEXT_PUBLIC_HOST}${endpoint}`;
 
   //? Fetching the product list
-  const response = await fetch(url, {
-    // headers: {
-    //   Cookie: connectSidCookie ? cookie : "",
-    // },
-  });
+  const response = await fetch(url);
 
   //? If there was an error, then no products are displayed
   if (!response.ok)
@@ -75,13 +67,6 @@ export const getServerSideProps = async (context) => {
 
   //? Converting the response to js object
   const jsonResponse = await response.json();
-  // const list = jsonResponse.products;
-  // if (jsonResponse.user) store.dispatch(initUser(jsonResponse.user));
-  // else if (connectSidCookie)
-  //   context.res.setHeader(
-  //     "Set-cookie",
-  //     "connect.sid=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
-  //   );
 
   //? If here is a user
   //? Here it is necessary to decode a user object, recieved from cookie
@@ -101,6 +86,4 @@ export const getServerSideProps = async (context) => {
       list: jsonResponse,
     },
   };
-  //   }
-  // );
 };
